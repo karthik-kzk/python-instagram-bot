23ec8e8996c5f20dfdf3bf6d5e029a392c703cf3
karthik-kzk <69966711+karthik-kzk@users.noreply.github.com>
2025-02-17 18:48:23 +0530

    Feat: folderDelete,count for files upload/download

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from instagrapi import Client
+from delete_folder_contents import delete_folder_contents
 from redditScraper import redditScraper
 from videoDownloader import videoDownloader
 import sys
@@ -7,13 +8,18 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
+folder_path = "media/"
+delete_folder_contents(folder_path)
+
+
 time_filter = "day"  # Options: "hour", "day", "week", "month", "year", "all"
 outputArrayLimit=5
 fetchPostLimit=5
 topPost=redditScraper(time_filter,outputArrayLimit,fetchPostLimit)
 # print(topPost)
-for post in topPost:
-    videoDownloader(post)    
+for index, post in enumerate(topPost, start=1):
+    videoDownloader(post) 
+    print(f"Download {index} / {len(topPost)}")   
 
 # sys.exit()
 cl = Client()
@@ -23,10 +29,11 @@ INSTAGRAM_PASSWORD=os.getenv('INSTAGRAM_PASSWORD')
 cl.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
 
 # user_id = cl.user_id_from_username(ACCOUNT_USERNAME)
-for post in topPost:
-    media = cl.clip_upload(
+for index, post in enumerate(topPost, start=1):
+    cl.clip_upload(
     f"media/{post["title"]}.mp4",    
     f"{post["title"]}"    
 )
+    print(f"Upload {index} / {len(topPost)}")
 
-    media.dict()
+    # media.dict()
